384031d98a079e8e20878e763201cac2176e6056
김지수 <wltn2858@swu.ac.kr>
2023-03-06 14:48:38 +0900

    김지수: 그룹 애너그램

--- soojik/week1/그룹애너그램/Main.java
+++ soojik/week1/그룹애너그램/Main.java
@@ -20,31 +20,43 @@ public class Main {
     int len = input_str.length;
     HashMap<String, List<String>> map = new HashMap<>();
 
-    // 문자열 정렬 후 sorted_str 배열에 순서 그대로 저장
+    // input 문자열 배열 순회
     for (int i = 0; i < len; i++) {
+      // 각 문자열 정렬
       char[] tmp = input_str[i].toCharArray();
       Arrays.sort(tmp);
       List<String> list_tmp;
 
-      // map에 있는 경우
+      // 정렬된 문자열 tmp가 map에 있는 경우
+      // 같은 구성을 가진 문자열이 1개 이상 있었다는 의미
+      // map에서 이제까지 나왔던 같은 구성의 단어를 모은 리스트 가져온다.
       if (map.containsKey(new String(tmp))) {
         list_tmp = map.get(new String(tmp));
-      } else {
+      }
+      // 정렬된 문자열 tmp가 map에 없는 경우
+      // 같은 구성의 문자열이 없었다는 의미
+      // list_tmp은 빈 리스트로 정의
+      else {
         list_tmp = new ArrayList<>();
       }
+      // 앞에서 만들어진 list_tmp에 현재 문자열을 넣는다.
       list_tmp.add(input_str[i]);
+      // 완성된 list_tmp를 map에 추가한다.
       map.put(new String(tmp), list_tmp);
     }
 
     int map_size = map.size();
 
+    // 정답 출력할 List 배열
     List[] answer = new ArrayList[map_size];
 
     int idx = 0;
+    // map 돌며 answer 배열에 list 추가해준다.
     for (String key : map.keySet()) {
       answer[idx++] = map.get(key);
     }
 
+    // 답 출력
     System.out.println(Arrays.toString(answer));
   }
 }
